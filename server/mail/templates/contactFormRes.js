2bee1d460e7a595caeee3f0af624daaa530a9e55
Deepak Kumar <i.deepak0808@gmail.com>
2023-12-07 21:37:13 +0530

    updated

--- server/mail/templates/contactFormRes.js
+++ server/mail/templates/contactFormRes.js
@@ -72,8 +72,6 @@ exports.contactUsEmail = (
     
     <body>
         <div class="container">
-            // <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                    // src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
             <div class="message">Contact Form Confirmation</div>
             <div class="body">
                 <p>Dear ${fullname},</p>
@@ -87,7 +85,7 @@ exports.contactUsEmail = (
                 <p>We appreciate your interest and will get back to you shortly. </p>
             </div>
             <div class="support">If you have any further questions or need immediate assistance, please feel free to reach
-                out to us at <a href="mailto: i.deepak0808@gmail.com">i.deepak0808@gmail.com</a>. We are here to help!</div>
+                out to us at <a href="mailto: jobsmela.info@gmail.com">jobsmela.info@gmail.com</a>. We are here to help!</div>
         </div>
     </body>
     
--- server/mail/templates/emailVerificationTemplate.js
+++ server/mail/templates/emailVerificationTemplate.js
@@ -69,14 +69,14 @@ const otpTemplate = (otp) => {
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
-				<p>Thank you for registering with Smart Job Search Portal. To complete your registration, please use the following OTP
+				<p>Thank you for registering with Jobs Mela. To complete your registration, please use the following OTP
 					(One-Time Password) to verify your account:</p>
 				<h2 class="highlight">${otp}</h2>
 				<p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
 				Once your account is verified, you will have access to our platform and its features.</p>
 			</div>
 			<div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-					href="mailto:i.deepak0808@gmail.com">i.deepak0808@gmail.com.com</a>. We are here to help!</div>
+					href="mailto:jobsmela.info@gmail.com">jobsmela.info@gmail.com</a>. We are here to help!</div>
 		</div>
 	</body>
 	
